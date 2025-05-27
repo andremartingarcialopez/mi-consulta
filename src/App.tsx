857001505532cdb71689696342b1e890947267ac
Andy <andy_garlop4@hotmail.com>
2025-05-26 21:39:27 -0600

    Actualizacion de logo en el header

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <header className="sticky top-0 z-50 glass  flex justify-center items-center border border-pink-400/50">
-      <img src='/public/Garlo.png' className="w-40 md:w-80 m-0" alt="imagen-logo-garlop"/>
+      <img src='/Garlo.png' className="w-40 md:w-80 m-0" alt="imagen-logo-garlop"/>
       </header>
       <div className=" mt-5 md:mt-10 mx-auto max-w-6xl grid grid-cols-1 lg:grid-cols-2 justify-around">
         <Form />
